1cd60979eaf0d6a3c89af479b9affc911ae4b79d
Ishan <96539755+Ishankp@users.noreply.github.com>
2025-02-11 02:33:38 +0000

    commit

--- main.cpp
+++ main.cpp
@@ -1,13 +1,13 @@
 
 #include <iostream> 
-#include <opencv2/opencv.hpp> // this folder will enable the user to manipulate images
+//#include <opencv2/opencv.hpp> // this folder will enable the user to manipulate images
 
 
 //Purpose: The main function in this code will act as the user interface and the basis of how we are going
 //to test out different functions.
 int main() {
     // user will do image input we will be working with simple black and white images to start this project off
-
-
+    std::cout << "Hello, World!" << std::endl;
+    return 0; // Indicate successful execution
     
 }
